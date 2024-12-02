a612fbd89ce72e1f7e457e96987fd840f281adf8
Melvin Castillo <78450770+Melvin080@users.noreply.github.com>
2024-12-02 12:41:25 -0500

    js cambios

--- asset/js/scrip.js
+++ asset/js/scrip.js
@@ -1,6 +1,6 @@
 const carrito = document.getElementById('carrito');
 const elementos1 = document.getElementById('lista-1');
-const lista = document.getElementById('#lista-carrito tbody');
+const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
 cargarEventListeners();
